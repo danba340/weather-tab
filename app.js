751a1e4fab06074f04e5be05ebfb0c8ed1e84534
danba340 <danba340@gmail.com>
2020-06-14 12:14:29 +0200

    Done

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-const POLL_INTERVAL = 60000; // 1 min in ms
 const URL_BASE = `https://api.openweathermap.org/data/2.5/forecast?appid=17811b814f836419545653077fa493fe`;
-const MOONS = ["🌑", "🌒", "🌓", "🌔", "🌕", "🌖", "🌗", "🌘"];
-let moonIndex = 0;
-const WEATHER_TO_EMOJI = {
-  Clear: "🌞",
-  Clouds: "⛅︎",
-  Thunder: "🌩",
-  Rain: "🌧",
-};
-let loadingInterval;
-let pollingInterval;
-let userPos;
+const setLoading = loadingHandler();
+const getWeatherString = weatherStringCreator();
 
-async function getWeather(pos) {
+function initApp(callback) {
+  function success(position) {
+    callback({ err: null, pos: position.coords });
+  }
+
+  function error() {
+    callback({ err: "Unable to retrieve your location, did you allow it?" });
+  }
+
+  if (!navigator.geolocation) {
+    callback({ err: "Geolocation is not supported by your browser" });
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+
+function onGeoLocationResponse(res) {
+  if (!res.err) {
+    fetchWeather(res.pos);
+    setInterval(() => {
+      fetchWeather(res.pos);
+    }, 3600000); // Once again every hour
+  } else if (res.err) {
+    alert(res.err);
+  } else {
+    alert("Unknown error");
+  }
+}
+
+async function fetchWeather(pos) {
   setLoading(true);
   const url = URL_BASE + `&lat=${pos.latitude}&lon=${pos.longitude}`;
   const weatherData = await (await fetch(url)).json();
@@ -20,85 +39,76 @@ async function getWeather(pos) {
 }
 
 function onWeatherData(weatherData) {
-  const dailyWeather = parseWeatherData(weatherData.list);
-  console.log(dailyWeather);
+  document.getElementById("location").innerHTML = `🗺 ${weatherData.city.name}`;
+  const dailyWeather = toDailyDescription(weatherData.list);
   const weatherString = createWeatherString(dailyWeather);
   setLoading(false);
   document.title = weatherString;
 }
 
-function loadingMoonAnimation() {
-  moonIndex = moonIndex + 1;
-  if (moonIndex === MOONS.length) {
-    moonIndex = 0;
+function weatherStringCreator(dailyWeather) {
+  const weatherToEmoji = {
+    Clear: "🌞",
+    Clouds: "⛅︎",
+    Thunder: "🌩",
+    Rain: "🌧",
+  };
+  function getComingDays() {
+    const allDays = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let dayIndex = new Date().getDay();
+    const fiveDays = [];
+    for (let i = 0; i < 5; i++) {
+      fiveDays.push(allDays[dayIndex]);
+      dayIndex = dayIndex < 6 ? dayIndex + 1 : 0;
+    }
+    return fiveDays.map((day) => day[0]);
   }
-  document.title = `${MOONS[moonIndex]}Loading...`;
+  return function getWeatherString() {
+    const weekDays = getComingDays();
+    const weatherString = weekDays
+      .map((day, index) => day + weatherToEmoji[dailyWeather[index]])
+      .join("");
+    return weatherString;
+  };
 }
 
-function setLoading(isLoading) {
-  if (isLoading) {
-    loadingInterval = setInterval(loadingMoonAnimation, 200);
-  } else {
-    clearInterval(loadingInterval);
+function loadingHandler() {
+  const moons = ["🌑", "🌒", "🌓", "🌔", "🌕", "🌖", "🌗", "🌘"];
+  let loadingInterval;
+  let moonIndex = 0;
+
+  function loadingMoonAnimation() {
+    moonIndex = moonIndex + 1;
+    if (moonIndex === moons.length) {
+      moonIndex = 0;
+    }
+    document.title = `${moons[moonIndex]}Loading...`;
   }
+
+  return function setLoading(isLoading) {
+    if (isLoading) {
+      loadingInterval = setInterval(loadingMoonAnimation, 200);
+    } else {
+      clearInterval(loadingInterval);
+    }
+  };
 }
 
-function getComingDays() {
-  const allDays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let dayIndex = new Date().getDay();
-  const fiveDays = [];
-  for (let i = 0; i < 5; i++) {
-    fiveDays.push(allDays[dayIndex]);
-    dayIndex = dayIndex < 6 ? dayIndex + 1 : 0;
-  }
-  return fiveDays.map((day) => day[0]);
-}
-
-function createWeatherString(dailyWeather) {
-  const weekDays = getComingDays();
-
-  const weatherString = weekDays
-    .map((day, index) => day + WEATHER_TO_EMOJI[dailyWeather[index]])
-    .join("");
-  return weatherString;
-}
-
-function parseWeatherData(payload) {
-  const dailyWeathers = payload
+function toDailyDescription(weatherEntries) {
+  const dailyDescriptions = weatherEntries
     .filter((dataPoint) => dataPoint.dt_txt.includes("15:00:00"))
     .map((day) => {
       return day.weather[0].main;
     });
-  return dailyWeathers;
+  return dailyDescriptions;
 }
 
-function getGeolocation() {
-  function success(position) {
-    userPos = position.coords;
-    getWeather(position.coords);
-  }
-
-  function error() {
-    alert("Unable to retrieve your location");
-  }
-
-  if (!navigator.geolocation) {
-    alert("Geolocation is not supported by your browser");
-  } else {
-    navigator.geolocation.getCurrentPosition(success, error);
-  }
-}
-
-getGeolocation();
-
-setInterval(() => {
-  getWeather(userPos);
-}, 3600000); // Once an hour
+initApp(onGeoLocationResponse);
